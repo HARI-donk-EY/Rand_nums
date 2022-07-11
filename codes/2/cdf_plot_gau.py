87f18a8411d12c7bcd492313fbd2d3a7334e6f73
HariVamshi6000 <ai21btech11014@iith.ac.in>
2022-07-11 14:29:40 +0530

    edited plot codes for cdf in 1 and 2

--- codes/2/cdf_plot_gau.py
+++ codes/2/cdf_plot_gau.py
@@ -1,6 +1,7 @@
 #Importing numpy, scipy, mpmath and pyplot
 import numpy as np
 import matplotlib.pyplot as plt
+import mpmath as mp
 
 #if using termux
 #import subprocess
@@ -8,10 +9,11 @@ import matplotlib.pyplot as plt
 #end if
 
 
-
+x1 = np.linspace(-4,4,300)
 x = np.linspace(-4,4,30)#points on the x axis
 simlen = int(1e6) #number of samples
 err = [] #declaring probability list
+the_cdf = []
 #randvar = np.random.normal(0,1,simlen)
 #randvar = np.loadtxt('uni.dat',dtype='double')
 randvar = np.loadtxt('gau.dat',dtype='double')
@@ -20,9 +22,14 @@ for i in range(0,30):
 	err_n = np.size(err_ind) #computing the probability
 	err.append(err_n/simlen) #storing the probability values in a list
 
+def Q_function(x):
+    return 0.5 * mp.erfc(x/(2**0.5))
+
+for i in x1:
+    the_cdf.append(1 - Q_function(i))
 
 plt.plot(x,err,'o')	
-plt.plot(x.T,err)#plotting the CDF
+plt.plot(x1, the_cdf)#plotting the CDF
 plt.grid() #creating the grid
 plt.xlabel('$x$')
 plt.ylabel('$F_X(x)$')
--- codes/3/cdf_plot_vdat.py
+++ codes/3/cdf_plot_vdat.py
@@ -1,20 +1,37 @@
  #Importing numpy, scipy, mpmath and pyplot
 import numpy as np
 import matplotlib.pyplot as plt
+import mpmath as mp
+import scipy
 
-
+x1 = np.linspace(-4,4,300)
 x = np.linspace(-4,4,30)#points on the x axis
 simlen = int(1e6) #number of samples
 err = [] #declaring probability list
+the_cdf = []
 randvar = np.loadtxt('vdat.dat',dtype='double')
 for i in range(0,30):
 	err_ind = np.nonzero(randvar < x[i]) #checking probability condition
 	err_n = np.size(err_ind) #computing the probability
 	err.append(err_n/simlen) #storing the probability values in a list
 
+def exxp(x):
+	return (1-np.exp(-0.5 *	x))
+
+cdf_graph = scipy.vectorize(exxp)
+
+def nfunc(x):
+	if x<0:
+		return 0
+	else:
+		return (cdf_graph(x))
+
+
+for i in x1:
+    the_cdf.append(nfunc(i))
 
 plt.plot(x,err,'o')	
-plt.plot(x.T,err)#plotting the CDF
+plt.plot(x1, the_cdf)
 plt.grid() #creating the grid
 plt.xlabel('$x$')
 plt.ylabel('$F_X(x)$')
