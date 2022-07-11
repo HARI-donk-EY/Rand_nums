ad8573abbd890812f285999c68b7b572f57374a6
HariVamshi6000 <ai21btech11014@iith.ac.in>
2022-07-11 12:52:34 +0530

    edited pfd_plot in 2

--- codes/2/pdf_plot.py
+++ codes/2/pdf_plot.py
@@ -4,20 +4,22 @@ import mpmath as mp
 import scipy 
 import matplotlib.pyplot as plt
 
-x = np.linspace(-6,6,50)#points on the x axis
+maxrange=50
+maxlim=6.0
+x = np.linspace(-maxlim,maxlim,maxrange)#points on the x axis
 simlen = int(1e6) #number of samples
 err = [] #declaring probability list
 pdf = [] #declaring pdf list
-h = 2*6/(50-1);
+h = 2*maxlim/(maxrange-1);
 randvar = np.loadtxt('gau.dat',dtype='double')
 
-for i in range(0,50):
+for i in range(0,maxrange):
 	err_ind = np.nonzero(randvar < x[i]) #checking probability condition
 	err_n = np.size(err_ind) #computing the probability
 	err.append(err_n/simlen) #storing the probability values in a list
 
 	
-for i in range(0,49):
+for i in range(0,maxrange-1):
 	test = (err[i+1]-err[i])/(x[i+1]-x[i])
 	pdf.append(test) #storing the pdf values in a list
 
@@ -26,7 +28,7 @@ def gauss_pdf(x):
 	
 vec_gauss_pdf = scipy.vectorize(gauss_pdf)
 
-plt.plot(x[0:(49)].T,pdf,'o')
+plt.plot(x[0:(maxrange-1)].T,pdf,'o')
 plt.plot(x, vec_gauss_pdf(x))#plotting the CDF
 plt.grid() #creating the grid
 plt.xlabel('$x_i$')
