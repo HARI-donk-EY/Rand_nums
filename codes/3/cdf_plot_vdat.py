36fd2ebd737c33f1f5964113f1ff3851b8127a0a
HariVamshi6000 <ai21btech11014@iith.ac.in>
2022-07-11 13:29:58 +0530

    removed useless lines in cdf_plot in 3

--- codes/3/cdf_plot_vdat.py
+++ codes/3/cdf_plot_vdat.py
@@ -2,18 +2,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#if using termux
-#import subprocess
-#import shlex
-#end if
-
-
 
 x = np.linspace(-4,4,30)#points on the x axis
 simlen = int(1e6) #number of samples
 err = [] #declaring probability list
-#randvar = np.random.normal(0,1,simlen)
-#randvar = np.loadtxt('uni.dat',dtype='double')
 randvar = np.loadtxt('vdat.dat',dtype='double')
 for i in range(0,30):
 	err_ind = np.nonzero(randvar < x[i]) #checking probability condition
@@ -27,14 +19,7 @@ plt.grid() #creating the grid
 plt.xlabel('$x$')
 plt.ylabel('$F_X(x)$')
 plt.legend(["Numerical", "Theory"])
-#if using termux
-#plt.savefig('../figs/uni_cdf.jpg')
-#plt.savefig('../figs/uni_cdf.eps')
-#subprocess.run(shlex.split("termux-open ../figs/uni_cdf.pdf"))
-#if using termux
+
 plt.savefig('fig/vdat_cdf.png')
-#plt.savefig('fig/gauss_cdf.eps')
-#subprocess.run(shlex.split("termux-open ../figs/gauss_cdf.pdf"))
-#else
 plt.show() #opening the plot window
 
